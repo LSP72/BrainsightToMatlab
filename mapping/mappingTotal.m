99b369e2afe4120edcdab6255620eed58ad1d6b2
Math <math.math@caramail.fr>
2025-10-01 15:14:03 -0400

    mappingTotal > plotting the mean maps

--- mapping/mappingTotal.m
+++ mapping/mappingTotal.m
@@ -53,28 +53,33 @@ if n > 1
     %% Plotting the average map
 
     % When using real coordinate of each session
-    % XX = zeros(size(coord.X.(Sessions(1))));
-    % YY = zeros(size(coord.Y.(Sessions(1))));
-    % ZZ = zeros(size(coord.Z.(Sessions(1))));
-    % PPP = zeros(size(coord.PP.(Sessions(1))));
+    XX = zeros(size(coord.X.(Sessions(1))));
+    YY = zeros(size(coord.Y.(Sessions(1))));
+    ZZ = zeros(size(coord.Z.(Sessions(1))));
+    PPP = zeros(size(coord.PP.(Sessions(1))));
 
-    % for i = 1:n
-    %     XX = XX + coord.X.(Sessions(i));
-    %     YY = YY + coord.Y.(Sessions(i));
-    %     ZZ = ZZ + coord.Z.(Sessions(i));
-    %     PPP = PPP + coord.PP.(Sessions(i));
-    % end
-    % XX = XX/n ;
-    % YY = YY/n;
-    % ZZ = ZZ/n;
-    % PPP = PPP/n;
-    % 
-    % figure
-    % plottingTheMap(XX, YY, ZZ, PPP, muscle)
-    % hold on
+    for i = 1:n
+        XX = XX + coord.X.(Sessions(i));
+        YY = YY + coord.Y.(Sessions(i));
+        ZZ = ZZ + coord.Z.(Sessions(i));
+        PPP = PPP + coord.PP.(Sessions(i));
+    end
+    XX = XX/n ;
+    YY = YY/n;
+    ZZ = ZZ/n;
+    PPP = PPP/n;
 
     % When plotting the average map from the target coordinates
-    
+    PPP_t = zeros(size(coord.PP_t.(Sessions(1))));
+    for i = 1:n
+        PPP_t = PPP_t + coord.PP_t.(Sessions(i));
+    end
+    PPP_t = PPP_t/n;
+
+    figure
+    plotting2DMap(X_t, Y_t, PPP_t, muscle)
+    figure
+    plottingTheMap(XX, YY, ZZ, PPP, muscle)
 
 
 else
@@ -87,6 +92,12 @@ else
     coord.Y.(sess) = Y;
     coord.Z.(sess) = Z;
     coord.PP.(sess) = PP;
+    [X_t, Y_t, PP_t] = collectingTargetCoord(data);
+    coord.X_t.(sess) = X_t;
+    coord.Y_t.(sess) = Y_t;
+    coord.PP_t.(sess) = PP_t;
+    figure
+    plotting2DMap(X_t, Y_t, PP_t, muscle)
     figure
     plottingTheMap(X, Y, Z, PP, muscle, 1)
 
