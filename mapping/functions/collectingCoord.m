9debd379f27a8746d43542503478eff881861f9b
Math <math.math@caramail.fr>
2025-10-05 21:37:26 -0400

    feat(collectingCoord): adding a way to collect the 2nd EMG

--- mapping/functions/collectingCoord.m
+++ mapping/functions/collectingCoord.m
@@ -1,10 +1,11 @@
-function [X, Y, Z, PP] = collectingCoord(data, muscle)
+function [X, Y, Z, PP_1] = collectingCoord(data, muscle)
 
 % function
 
     % Collecting all the dot of the grid
     grid = struct();
     % m = ;
+    fieldNames = fieldnames(data.samples{1,1});
 
     for i = 1:length(data.samples)
         if ~isempty(data.samples{1, i}.Assoc__Target)
@@ -14,9 +15,15 @@ function [X, Y, Z, PP] = collectingCoord(data, muscle)
                 grid.(sampleName).Loc_X = data.samples{1, i}.Loc__X;
                 grid.(sampleName).Loc_Y = data.samples{1, i}.Loc__Y;
                 grid.(sampleName).Loc_Z = data.samples{1, i}.Loc__Z;
-                grid.(sampleName).EMG_PP = data.samples{1, i}.EMG_Peak_to_peak_1;
-                grid.(sampleName).Latency = data.samples{1, i}.EMG_Latency_1;
                 grid.(sampleName).Assoc_Target = data.samples{1, i}.Assoc__Target;
+                grid.(sampleName).EMG_PP_1 = data.samples{1, i}.EMG_Peak_to_peak_1;
+                grid.(sampleName).Latency_1 = data.samples{1, i}.EMG_Latency_1;
+                if  ismember('EMG_Peak_to_Peak_2', fieldNames) 
+                    grid.(sampleName).EMG_PP_2 = data.samples{1, i}.EMG_Peak_to_Peak_2;
+                end
+                if ismember('EMG_Latency_2', fieldNames)
+                    grid.(sampleName).Latency_2 = data.samples{1, i}.EMG_Latency_2;
+                end
             end
         else
             continue
@@ -44,13 +51,17 @@ function [X, Y, Z, PP] = collectingCoord(data, muscle)
     X = [];
     Y = [];
     Z = [];
-    PP = [];
+    PP_1 = [];
+    PP_2 = [];
     
     for i = 1:length(grid_points)
         X = [X, grid.(grid_points{i}).Loc_X];
         Y = [Y, grid.(grid_points{i}).Loc_Y];
         Z = [Z, grid.(grid_points{i}).Loc_Z];
-        PP = [PP, grid.(grid_points{i}).EMG_PP];
+        PP_1 = [PP_1, grid.(grid_points{i}).EMG_PP_1];
+        if ismember('EMG_PP_2', fields)
+            PP_2 = [PP_2, grid.(grid_points{i}).EMG_PP_2];
+        end
     end
     
 end
